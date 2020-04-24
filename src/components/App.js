2b5d9eb8cd7f0a0ba6d5fd016cc79a03ace2b5ae
Freeda Vales <freedavales28@fmail.com>
2020-04-24 19:04:47 +0530

    Route Authentication

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,48 @@
 import React from "react";
 import Header from "./common/Header";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import LoginPage from "./LogIn/LoginPage";
 import { connect } from "react-redux";
 import { handleInitialData } from "../redux/actions/shared";
 import Dashboard from "./PollsPage/Dashboard";
 import NewPoll from "./PollsPage/NewPoll";
 import Leaderboard from "./Leaderboard/Leaderboard";
+import PageNotFound from "./common/PageNotFound";
 
 class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
 
+  PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={props =>
+        this.props.authedUser === null ? (
+          <Redirect to="/login" />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
+
   render() {
     return (
       <>
         <Header name={this.props.name} />
         <div className="container">
-          {this.props.authedUser === null ? (
+          <Switch>
             <Route path="/login" exact component={LoginPage} />
-          ) : (
-            <Switch>
-              <Route exact path="/" component={Dashboard} />
-              <Route exact path="/add" component={NewPoll} />
-              <Route exact path="/leaderboard" component={Leaderboard} />
-              {/* <Route path="/dashboard">
-                <LoginPage />
-              </Route>
-              <Route path="*">
-                <LoginPage />
-              </Route> */}
-            </Switch>
-          )}
+            <this.PrivateRoute exact path="/" component={Dashboard} />
+            <this.PrivateRoute exact path="/add" component={NewPoll} />
+            <this.PrivateRoute
+              exact
+              path="/leaderboard"
+              component={Leaderboard}
+            />
+            <this.PrivateRoute component={PageNotFound} />
+          </Switch>
         </div>
       </>
     );
---
+++ src/components/common/PageNotFound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageNotFound = props => {
+  return <h3 className="text-center">Page Not Found</h3>;
+};
+
+export default PageNotFound;
